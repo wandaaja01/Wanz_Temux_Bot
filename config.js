860b9cc0eb59f6a9a8de1c7ebc79c01398e73a61
Wanz Official <147049837+wandaaja01@users.noreply.github.com>
2025-06-11 01:07:03 +0700

    config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  botToken: 'BOT_TOKEN_TELE',
+  github: {
+    token: 'TOKEN_GITHUB_KALIAN',
+    apiUrl: 'https://api.github.com/repos/(nama_github)/(nama_repo)/contents/(nama_file)'
+  }
+}
